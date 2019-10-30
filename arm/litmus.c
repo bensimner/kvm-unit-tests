1c5d39e14a4f95f393f9f77a6dd4bc62f28d845b
Ben Simner <benjsimner@gmail.com>
2019-10-30 15:09:19 +0000

    Add MP+dmb+eret0

--- arm/litmus.c
+++ arm/litmus.c
@@ -8,20 +8,22 @@
 extern int MyMP_pos(void);
 extern int MyMP_dmbs(void);
 extern int MyMP_dmb_svc(void);
+extern int MyMP_dmb_eret0(void);
 extern int MyMP_dmb_svc0(void);
 extern int MyMP_dmb_svc_eret(void);
 
 int main(int argc, char **argv)
 {
-    /* MP(argc, argv); */
-    /* MP_dmbs(argc, argv); */
-    /* MP_dmb_svc(argc, argv); */
-    /* MP_dmb_svc_eret(argc, argv); */
-    MyMP_pos();
-    MyMP_dmbs();
-    MyMP_dmb_svc();
-    MyMP_dmb_svc0();
-    MyMP_dmb_svc_eret();
-    return report_summary();
+  /* MP(argc, argv); */
+  /* MP_dmbs(argc, argv); */
+  /* MP_dmb_svc(argc, argv); */
+  /* MP_dmb_svc_eret(argc, argv); */
+  MyMP_pos();
+  MyMP_dmbs();
+  MyMP_dmb_svc();
+  MyMP_dmb_svc0();
+  MyMP_dmb_eret0();
+  MyMP_dmb_svc_eret();
+  return report_summary();
 }
 
